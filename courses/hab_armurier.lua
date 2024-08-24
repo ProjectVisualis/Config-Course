3d47828bb5d76a934eef84975a3849e71798a255
Lucasgood5 <65530145+Lucasgood5@users.noreply.github.com>
2024-08-24 10:03:15 +0200

    requirement for habilitation

--- courses/hab_armurier.lua
+++ courses/hab_armurier.lua
@@ -4,5 +4,13 @@ COURSE.name = "Habilitation d'armurier"
 COURSE.icon = "icon16/user.png"
 COURSE.description = "Habilitation pour être armurier."
 COURSE.requirement = {}
+COURSE.callback = function(client)
+    local character = client:GetCharacter()
+    if not character then return false end
+    if not (client:Team() == FACTION_SECURITY) then return false end
+
+    if character:GetRank() >1 then return true end -- Pas de recrue.
+    return false
+end
 
 return COURSE
