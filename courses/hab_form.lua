61860101700694d1c89ef1697209e52d82cd5d16
Lucasgood5 <65530145+Lucasgood5@users.noreply.github.com>
2024-08-24 10:03:15 +0200

    requirement for habilitation

--- courses/hab_form.lua
+++ courses/hab_form.lua
@@ -4,5 +4,14 @@ COURSE.name = "Habilitation de Formateur"
 COURSE.icon = "icon16/user.png"
 COURSE.description = "Habilitation pour être formateur."
 COURSE.requirement = {}
+COURSE.callback = function(client)
+    local character = client:GetCharacter()
+    if not character then return false end
+    if not (client:Team() == FACTION_SECURITY) then return false end
+
+    if character:GetRank() >3 then return true end -- Pas de soldat.
+    return false
+end
+
 
 return COURSE
